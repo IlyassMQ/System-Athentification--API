43c05ea7ba39394d66bbde5a01fc97d85117f5fd
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-03-11 11:57:31 +0000

    feat : Add user registration

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Hash;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request){
+      $user = User::create([
+            'name'=>$request->name,
+            'email' =>$request->email,
+            'password' => Hash::make($request->password),
+      ]);
+      return response()->json([
+        "message"=> "Account created with succes"
+      ],201);
+    } 
+}
