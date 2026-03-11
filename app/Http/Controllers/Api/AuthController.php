70b6bb51183c5fda9ace5b210079e2f7e8ba9d2b
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-03-11 14:54:08 +0000

    feat : Add user login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -20,4 +22,24 @@ class AuthController extends Controller
         "message"=> "Account created with succes"
       ],201);
     } 
+
+    public function login(LoginRequest $request){
+      $reqVal = $request->validated();
+      if (!Auth::attempt($reqVal)) {
+        return response()->json([
+          'status'=>'Error',
+          'message' => 'login failed email or password are incorrect',
+        ], 401);
+    }
+    $user = Auth::user();
+
+    $token = $user->createToken('api-token')->plainTextToken;
+
+    return response()->json([
+        'status'=>'Sucsses',    
+        'message' => 'Login success',
+        'token' => $token
+    ], 200);
+
+    }
 }
